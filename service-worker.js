fdca75fbbf74ee2287f1fb08a93385e0d2811032
seacall12-lab <seacall12@gmail.com>
2026-06-19 11:50:39 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var CACHE_NAME = "abyss-breaker-v2.0.0";
+var CORE_ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./data.js",
+  "./state.js",
+  "./game.js",
+  "./render.js",
+  "./icons.js",
+  "./ui.js",
+  "./main.js",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(CORE_ASSETS);
+    }).then(function () {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (key) {
+        return key === CACHE_NAME ? null : caches.delete(key);
+      }));
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      return cached || fetch(event.request).then(function (response) {
+        var copy = response.clone();
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, copy);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
